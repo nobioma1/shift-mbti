5c61c7278638c5f4eaea0cf70b6699dd1c344554
Noble Obioma <nobioma1@gmail.com>
2020-12-13 17:31:28 +0100

    create styled container for errors

---
+++ client/src/components/styled/Errors.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ErrorsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem 0;
+
+  p {
+    color: red;
+  }
+`;
