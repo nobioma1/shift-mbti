a1bb88a41ae91465af6b7dac32647df8c0a72283
Noble Obioma <nobioma1@gmail.com>
2020-12-13 17:29:40 +0100

    create header component

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { HeaderWrapper } from './header.styled';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <h2>Discover Your Perspective</h2>
+      <p>
+        Complete the 7 min test and get a detailed report of your lenses on the
+        world.
+      </p>
+    </HeaderWrapper>
+  );
+};
+
+export default Header;
---
+++ client/src/components/header/header.styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const HeaderWrapper = styled.div`
+  h2 {
+    font-size: 1.125rem;
+    font-weight: 500;
+    color: #113264;
+    margin-bottom: 1.125rem;
+  }
+
+  p {
+    font-size: 1rem;
+    color: #495057;
+  }
+`;
