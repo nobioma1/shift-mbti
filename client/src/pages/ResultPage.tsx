8977afbc57fc80ed5f6793d0ae4f63cf49b6e2ed
Noble Obioma <nobioma1@gmail.com>
2020-12-13 17:31:06 +0100

    create results page

---
+++ client/src/pages/ResultPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Result from '../components/result';
+
+const ResultPage = () => {
+  return (
+    <div>
+      <Result />
+    </div>
+  );
+};
+
+export default ResultPage;
