80b87c68efd05ee70d488bbc7e9e1fc56d7208c8
Noble Obioma <nobioma1@gmail.com>
2020-12-13 17:26:57 +0100

    create styled component CardSection

---
+++ client/src/shared/CardSection.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const CardSection = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #e5e5e5;
+  border-radius: 4px;
+  padding: 2rem 3rem;
+
+  h3 {
+    font-family: 'Nunito', sans-serif;
+    margin-bottom: 1.75rem;
+    font-weight: 700;
+    font-size: 1.125rem;
+    text-align: center;
+  }
+`;
